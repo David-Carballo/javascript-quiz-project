e692ab4a1b3080383d32881eee8fa3e0c189b306
David Carballo <dvd.cm6@gmail.com>
2024-09-02 17:08:46 +0200

    day 1 completed

--- src/question.js
+++ src/question.js
@@ -2,6 +2,19 @@ class Question {
     // YOUR CODE HERE:
     //
     // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty; //a number between 1 and 3, with 1 being the easiest and 3 being the hardest
+    }
 
     // 2. shuffleChoices()
+    shuffleChoices() {
+        for (let i = this.choices.length - 1; i > 0; i--) { 
+            const choiceRandom = Math.floor(Math.random() * (i + 1)); 
+            [this.choices[i], this.choices[choiceRandom]] = [this.choices[choiceRandom], this.choices[i]]; 
+        } 
+        return this.choices; 
+    }; 
 }
--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,60 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+    // 2. getQuestion() Returns the question from the questions array at the position of currentQuestionIndex
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+    }
     
-    // 3. moveToNextQuestion()
+    // 3. moveToNextQuestion() When called, increments the currentQuestionIndex by 1.
+    moveToNextQuestion(){
+        this.currentQuestionIndex++;
+    }
+    // 4. shuffleQuestions() Shuffles the elements stored in the questions array of the Quiz.
+    shuffleQuestions(){
+        for (let i = this.questions.length - 1; i > 0; i--) { 
+            const questionsRandom = Math.floor(Math.random() * (i + 1)); 
+            [this.questions[i], this.questions[questionsRandom]] = [this.questions[questionsRandom], this.questions[i]]; 
+        } 
+        return this.questions; 
+    }
 
-    // 4. shuffleQuestions()
+    // 5. checkAnswer(answer) Checks if the passed answer is correct for the current question and increments correctAnswers by 1 if the answer is correct.
+    checkAnswer(answer){
+        if(answer === this.questions[this.currentQuestionIndex].answer){
+            this.correctAnswers+=1;
+        }
+    }
 
-    // 5. checkAnswer(answer)
+    // 6. hasEnded() Returns true if the quiz has ended (the last question has been answered), and false otherwise
+    hasEnded() {
+        if(this.currentQuestionIndex < this.questions.length) return false;
+        else if (this.currentQuestionIndex === this.questions.length) return true;
+    }
 
-    // 6. hasEnded()
+    // 7. filterQuestionsByDifficulty(difficulty) Filters the questions array by the difficulty level passed as an argument.
+    filterQuestionsByDifficulty(difficulty){
+        if(difficulty >= 1 && difficulty <= 3) {
+            // array.filter(elemento => condición)
+            this.questions = this.questions.filter((question) => question.difficulty === difficulty);
+        }
+    }
+
+    //8. You should use the reduce() method to sum the difficulty of all the questions and then divide the sum by the number of questions to get the average difficulty.
+    averageDifficulty(){
+        // array.reduce( funcion, valor inicial)
+        // array.reduce( (acumulador, elemento) => acumulador + elemento, valor inicial )
+        let totalDifficulty = this.questions.reduce((accumulator, question) => accumulator + question.difficulty, 0);
+        //console.log(totalDifficulty);
+        return totalDifficulty/this.questions.length;
+    }
+
 }
+
